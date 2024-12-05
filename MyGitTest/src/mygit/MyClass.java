3add3f43a7f6b91f2527ceb9bf23dfe6b0ceb0f7
yangnail <wardingplz@naver.com>
2024-12-05 13:45:49 +0900

    dev branch test1

--- MyGitTest/src/mygit/MyClass.java
+++ MyGitTest/src/mygit/MyClass.java
@@ -4,5 +4,6 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("edited Hello Git!");
+		System.out.println("dev branch Hello Git!");
 		}
 }
